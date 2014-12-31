684b5b6896b5ef3fbc74b3e8201f3060182b2795
Xuebin Dong <xudong@redhat.com>
2014-12-31 15:25:06 +0800

    fix-bug: JQuery UI Sortable with Horizontal Scrolling is a mess

--- client/board/board.js
+++ client/board/board.js
@@ -2,12 +2,27 @@ Lists = new Meteor.Collection('lists');
 
 Template.board.rendered = function() {
   var _this = this;
-    Meteor.subscribe('lists');
+  var boardId = _this.data._id;
+  Meteor.subscribe('lists');
   var data = function() {
-    return Lists.find({ boardId: _this.data._id });
+    return Lists.find({ boardId: boardId });
   };
   var tmpl = function() {
     return Template.list;
   };
   Blaze.render(Blaze.Each(data, tmpl), this.$('#board-content')[0]);
+
+
+  var $sortableList = $('#board-content').sortable({
+    axis: 'x',
+    placeholder: 'list-placeholder',
+    tolerance: 'pointer',
+    stop: function(event, ui) {
+      var list_order = $sortableList.sortable('toArray').join(',');
+      Boards.update(boardId, { $set: {list_order: list_order, moved_list_id: ui.item[0].id }});
+    }
+  })
+  .disableSelection();
+
+
 };

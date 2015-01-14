8faf8e11bd9b69bdcf1df963894faa406f2eedac
Xuebin Dong <xudong@redhat.com>
2015-01-14 17:18:04 +0800

    add loading spinner

--- client/main.js
+++ client/main.js
@@ -14,6 +14,7 @@ Router.map(function () {
       }
       return boards.map(clearifyId);
     },
+    loadingTemplate: 'loading',
     waitOn: function () {
       return Meteor.subscribe('boards');
     },

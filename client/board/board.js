b53d0ad3da6dfb60f74ce74c8044ed4939b67760
Xuebin Dong <xudong@redhat.com>
2015-01-15 15:09:32 +0800

    complement the style modification for font awesome

--- client/board/board.js
+++ client/board/board.js
@@ -120,10 +120,10 @@ Template.board.events({
     }
   },
   'mouseenter .close-btn': function(evnet) {
-    $(evnet.target).removeClass('icon-remove-circle').addClass('icon-remove-sign');
+    $(evnet.target).removeClass('fa-times-circle').addClass('fa-times-circle-o');
   },
   'mouseleave .close-btn': function(evnet) {
-    $(evnet.target).removeClass('icon-remove-sign').addClass('icon-remove-circle');
+    $(evnet.target).removeClass('fa-times-circle-o').addClass('fa-times-circle');
   },
   'click .close-btn': function(evnet, template) {
     var $boardFooter = $('#board-footer');
